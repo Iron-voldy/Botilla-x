fb349525dd2ba112bc983976499eb21718e438e7
Dulshan75 <80284053+Dulshan75@users.noreply.github.com>
2021-03-14 17:56:24 +0530

    Update welcome.py

--- DaisyX/modules/welcome.py
+++ DaisyX/modules/welcome.py
@@ -186,7 +186,7 @@ def new_member(update: Update, context: CallbackContext):
             # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
                 update.effective_message.reply_text(
-                    "Oh my boss [😋](https://telegra.ph/file/208c443c5916abac6d5e6.jpg)! Hey Mr.matheesha official🙋‍♀, let's get this moving (｡•̀ᴗ-)✧.",
+                    "Oh My creation[❤] Opps! Sorry😉 Creater It was a joke (https://telegra.ph/file/a40f0a704bca2410c7ff9.jpg)! Hey Mr.CODELYOKOBOT🙋‍♀, let's get this moving (⚡)✧.",
                     reply_to_message_id=reply,
                 )
                 welcome_log = (
